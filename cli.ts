23c5f402d3e1c45a0ce290edbb7f3f04e3a943fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-09-21 15:39:27 +0900

    format

--- cli.ts
+++ cli.ts
@@ -1,11 +1,13 @@
 import { Server, TextDictionary } from "./mod.ts";
 import { parse } from "std/flags/mod.ts";
 
-const options = parse(Deno.args, { default: { port: 1178 } });
+const options = parse(Deno.args, {
+  default: {
+    port: 1178,
+    dictionary: "/usr/share/skk/SKK-JISYO.L",
+  },
+});
 const dictionary = new TextDictionary();
-await dictionary.load(
-  options.dictionary ?? "/usr/share/skk/SKK-JISYO.L",
-  "euc-jp",
-);
+await dictionary.load(options.dictionary, "euc-jp");
 const server = new Server(dictionary);
 server.listen(options as any);
