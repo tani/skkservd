a667aed02b511dc8317989a0b2cd9d6d79750473
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-09-22 08:30:07 +0900

    Okuri ari nasi

--- dictionary.ts
+++ dictionary.ts
@@ -6,31 +6,42 @@ export interface Dictionary {
 }
 
 export class TextDictionary implements Dictionary {
-  #table: Map<string, string[]> = new Map();
+  #okuriAri: Map<string, string[]> = new Map();
+  #okuriNasi: Map<string, string[]> = new Map();
   async load(path: string, encording = "euc-jp"): Promise<void> {
     const blob = await Deno.readFile(path);
-    const src = iconv.decode(blob, encording);
-    for (const match of src.matchAll(/^([^;].*) \/(.*)\/$/gm)) {
+    const [okuriAri, okuriNasi] = iconv.decode(blob, encording).split(";; okuri-nasi entries");
+    for (const match of okuriAri.matchAll(/^([^;].*) \/(.*)\/$/gm)) {
       const key = match[1];
       const value = match[2].split("/").map((c: string) =>
         c.replace(/;.*$/, "")
       );
-      this.#table.set(key, value);
+      this.#okuriAri.set(key, value);
+    }
+    for (const match of okuriNasi.matchAll(/^([^;].*) \/(.*)\/$/gm)) {
+      const key = match[1];
+      const value = match[2].split("/").map((c: string) =>
+        c.replace(/;.*$/, "")
+      );
+      this.#okuriNasi.set(key, value);
     }
   }
   get __debug() {
-    return this.#table;
+    return this.#okuriAri;
   }
   convert(entry: string): Promise<string[]> {
-    return new Promise((resolve) => resolve(this.#table.get(entry) ?? []));
+    return Promise.resolve([
+      ...this.#okuriAri.get(entry) ?? [],
+      ...this.#okuriNasi.get(entry) ?? []
+    ]);
   }
   complete(entry: string): Promise<string[]> {
     const cs: string[] = [];
-    for (const key of this.#table.keys()) {
+    for (const key of this.#okuriNasi.keys()) {
       if (key.startsWith(entry)) {
         cs.push(key);
       }
     }
-    return new Promise((resolve) => resolve(cs));
+    return Promise.resolve(cs);
   }
 }
